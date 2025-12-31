0dcfdaa0512cc4e881fee46d5011b51e83857258
estudante668 <desenhista668@gmail.com>
2025-12-31 12:36:20 -0300

    Melhorias

--- copia-arq.c
+++ copia-arq.c
@@ -1,12 +1,22 @@
 
+
 #include <stdio.h>
 
 int main() {
+
   int pegaCaracters;
+  int cont;
+
+  cont = 0;
   pegaCaracters = getchar();
+
   while( pegaCaracters != EOF ){
+      ++cont;
       putchar(pegaCaracters);
       pegaCaracters = getchar();
-  }
-  
-}
+} 
+
+   printf("%s %d\n", "Quantidade de caracteres: ", cont - 1);
+   return 0;  
+
+} 
